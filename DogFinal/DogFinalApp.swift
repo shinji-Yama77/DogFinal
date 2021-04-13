86cd50ddaa15c11728fcb76c2c0b2e05e5ba56eb
Shinji Yamashita <syamas@uw.edu>
2021-04-13 15:13:35 +0800

    Initial Commit

---
+++ DogFinal/DogFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinalApp.swift
+//  DogFinal
+//
+//  Created by shinji Yamashita on 2021/4/13.
+//
+
+import SwiftUI
+
+@main
+struct DogFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
